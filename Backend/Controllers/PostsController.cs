2a73f3f796f28b4ebc74d195ebb7012bb2ee1b5f
caaz <carlos.aguirre@it-seekers.com>
2025-04-11 15:51:12 -0600

    dtos conexion a un api

---
+++ Backend/Controllers/PostsController.cs
@@ -0,0 +1,22 @@
+﻿using Backend.DTOs;
+using Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        IPostsService _titleService;
+
+        public PostsController(IPostsService titleService)
+        {
+            _titleService = titleService;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<PostDto>> Get() =>
+            await _titleService.Get();
+    }
+}
