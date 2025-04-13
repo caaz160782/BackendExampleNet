ce924ef79c46cdeb428bf581e3a0f94708c682f5
Charles Aguirre <caaz160782@gmail.com>
2025-04-13 15:00:50 -0600

    auto mappers

--- Backend/Models/Beer.cs
+++ Backend/Models/Beer.cs
@@ -9,7 +9,7 @@ namespace Backend.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int BeerID { get; set; }
         public int BrandID { get; set; }
-        public string? BeerName { get; set; }
+        public string BeerName { get; set; }
         [Column(TypeName = "decimal(18,2)")]
         public decimal Alcohol { get;set; }
 
